95793d23e96786c3248638d9cdb3353677240b32
luizabsl <62751065+luizabsl@users.noreply.github.com>
2026-08-09 12:50:31 +0000

    criar classe calculadora, exportá-la e acessá-la a partir da classe teste

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,11 @@
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+  console.log(resultado);
+});
+
 test("nome de teste", callbackFunction); // referencia a definicao externa
 
 function callbackFunction() {
