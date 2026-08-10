b05bff87afe76d40a65d1af9d820d8aa559a397b
luizabsl <62751065+luizabsl@users.noreply.github.com>
2026-08-10 22:49:54 +0000

    inclui endpoint api/v1/status e cria teste integrado

---
+++ pages/api/v1/status.js
@@ -0,0 +1,9 @@
+// /api/status
+
+function status(request, response) {
+  response.status(200).json({ text: "Envio de status code" }); //assume charset como utf-8
+}
+
+export default status; //um arquivo pode ter várias funções, no export default eu
+//deixo explícito qual é a função de entrada da classe.
+// Ela que lida com o request e response desse endpoint
