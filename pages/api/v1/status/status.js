53a2727b4a2949539a1a0406117e970d799c3bec
luizabsl <62751065+luizabsl@users.noreply.github.com>
2026-08-15 20:54:45 +0000

    corrige posição do arq `status`

--- pages/api/v1/status/status.js
+++ pages/api/v1/status/status.js
@@ -1,6 +1,10 @@
 // /api/status
+import database from "../../../../infra/database.js";
 
-function status(request, response) {
+async function status(request, response) {
+  console.log(database);
+  const result = await database.query("SELECT 1 + 1 AS SOMA;");
+  console.log(result.rows);
   response.status(200).json({ text: "Envio de status code" }); //assume charset como utf-8
 }
 
